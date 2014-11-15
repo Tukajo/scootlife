fe37078e48552b1c00b46ec02992d3e825bd3985
deyog2228 <deyog2228@my.uwstout.edu>
2014-11-15 17:50:39 -0600

    Stupid text fix

--- src/scripts/gameengine.js
+++ src/scripts/gameengine.js
@@ -4313,63 +4313,49 @@ function totalPricePerChair(){
             //ctx2.drawImage(station[0].image,station[0].x,station[0].y,100,200);
             drawSprtSht();
             
+            ctx.fillStyle = "#FFF";
+            ctx.font = "25pt Arial";
             for (var i =0; i<9; i++) {
                 if (station[0].hover) {
                     ctx.drawImage(note.image, posx, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Saw", posx + 70, posy + 30);
                 }
                 if (station[1].hover) {
                     ctx.drawImage(note.image, posx, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Drill", posx + 70, posy + 30);
                     
                 }
                 if (station[2].hover) {
                     ctx.drawImage(note.image, posx - 200, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Bender", posx - 150, posy + 30);
                 }
                 if (station[3].hover) {
                     ctx.drawImage(note.image, posx, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Welder", posx + 50, posy + 30);
                 }
                 if (station[4].hover) {
                     ctx.drawImage(note.image, posx, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Grinder", posx + 50, posy + 30);
                 }
                 if (station[5].hover) {
                     ctx.drawImage(note.image, posx - 200, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Paint Booth", posx - 180, posy + 30);
                 }
                 if (station[6].hover) {
                     ctx.drawImage(note.image, posx, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Assembly", posx + 30, posy + 30);
                 }
                 if (station[7].hover) {
                     ctx.drawImage(note.image, posx, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Fabric Cutter", posx + 10, posy + 30);
                 }
                 if (station[8].hover) {
                     ctx.drawImage(note.image, posx - 200, posy, 200, 200);
-                    ctx.font = "25pt Arial";
-                    ctx.fillStyle = "#FFF";
                     ctx.fillText("Sewing", posx - 150, posy + 30);
                 }
             }
+            ctx.fillStyle = "#000";
+            ctx2.font = "10px Georgia";
 
             /*for (var i = 0; i < 9; i++) {
                 draw(ctx2, station[i], 3, -3);
