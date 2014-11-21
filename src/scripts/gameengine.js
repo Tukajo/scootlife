dda2c6501acc93a280140b78c2dd998558903bf7
MercenaryMcCloud <tolbertm0937@my.uwstout.edu>
2014-11-21 11:54:46 -0600

    Revert "Scaling m8"
    This reverts commit 994a2612d11150b68b9d781abf359d5e6d12805f.

--- src/scripts/gameengine.js
+++ src/scripts/gameengine.js
@@ -9,19 +9,10 @@ canvas.top = 0;
 canvas.left = 0;
 canvas.width = 1550;
 canvas.height = 750;
-
-var xScale=(1600/1550);
-var yScale=(1000/750);
-
-//ctx.scale(xScale,yScale);
-
 document.body.appendChild(canvas);
 var shadowForFactoryIcons = new Image();
 shadowForFactoryIcons.src = "Art_Assets/workshop_icons/iconDropShadow.png";
 var ctx2 = canvas.getContext("2d");
-
-//ctx2.scale(xScale,yScale);
-
 var monthsArray = {
     January: "January",
     February: "February",
@@ -745,7 +736,6 @@ function drawSprtSht() {
         }
 
     };
-
 
 
     var gameObject = function (x, y, h, w, src, altSrc) {
@@ -4631,9 +4621,9 @@ function totalPricePerChair(){
 
     var draw = function (canvas, object, xDis, yDis) {
         if (object.ready && !object.hover)
-            canvas.drawImage(object.image, (object.x*xScale), (object.y*yScale), (object.w*xScale), (object.h*yScale));
+            canvas.drawImage(object.image, object.x, object.y, object.w, object.h);
         if (object.readyAlt && object.hover)
-            canvas.drawImage(object.imageAlt, (object.x*xScale) + xDis, (object.y*yScale) + yDis, (object.w*xScale), (object.h*yScale));
+            canvas.drawImage(object.imageAlt, object.x + xDis, object.y + yDis, object.w, object.h);
     }
 
     var tracker = function (e) {
